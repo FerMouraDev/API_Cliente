8acd62f5eba60361a096d337122151d720599dde
Fernando M <147080357+FerMouraDev@users.noreply.github.com>
2026-03-27 17:13:22 -0300

    Criando interface

---
+++ Interface/ICliente.cs
@@ -0,0 +1,13 @@
+﻿using API_Cliente.Model;
+
+namespace API_Cliente.Interface
+{
+    public interface ICliente
+    {
+        Task<bool> inserir(Cliente cliente);
+        Task<bool> alterar(Cliente cliente);
+        Task<bool> deletar(Cliente cliente);
+        Task<List<Cliente>> buscar();
+        Task<Cliente> buscarPorId(int id);
+    }
+}
