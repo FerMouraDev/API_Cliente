2a5dd37671df24b634debef47f4fa055f76d585b
Fernando M <147080357+FerMouraDev@users.noreply.github.com>
2026-03-27 20:35:09 -0300

    Ajuste na controller

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -39,13 +39,28 @@ namespace API_Cliente.Controllers
             return BadRequest("Erro ao inserir cliente.");
         }
 
-        [HttpPut]
-        public async Task<ActionResult<bool>> Put([FromBody] Cliente cliente)
+        [HttpPut("{id}")] // Adicione o {id} na rota para o Axios te encontrar
+        public async Task<ActionResult<bool>> Put(int id, [FromBody] Cliente cliente)
         {
+            // Garante que o ID da URL é o mesmo do objeto (Segurança de Pleno!)
+            cliente.Id = id;
+
             var sucesso = await _clienteRepository.alterar(cliente);
             if (sucesso) return Ok("Cliente alterado com sucesso!");
 
-            return BadRequest("Erro ao inserir cliente.");
+            return BadRequest("Erro ao alterar cliente.");
+        }
+
+        [HttpDelete("{id}")] // Faltava este método na sua Controller!
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            // Criamos um objeto temporário para o Dapper entender o @Id
+            var cliente = new Cliente { Id = id };
+
+            var sucesso = await _clienteRepository.deletar(cliente);
+            if (sucesso) return Ok("Cliente deletado com sucesso!");
+
+            return BadRequest("Erro ao deletar cliente.");
         }
     }
 }
