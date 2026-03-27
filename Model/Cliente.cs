426b6decd9817a4c269a75ed2ac47b2cefb9bf25
Fernando M <147080357+FerMouraDev@users.noreply.github.com>
2026-03-27 16:59:11 -0300

    Criando uma class de cliente

---
+++ Model/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Cliente.Model
+{
+    public class Cliente
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
